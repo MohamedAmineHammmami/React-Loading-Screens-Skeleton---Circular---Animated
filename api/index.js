9b27be5df96fed1375dbcd2bf99c4034e603810a
Mohamed Amine Hammami <mohamedaminehammami1994@gmail.com>
2025-01-06 14:34:33 +0100

    backend

--- api/index.js
+++ api/index.js
@@ -1,9 +1,26 @@
 import express from "express";
+import { videos, user } from "./dummyData.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/videos", (req, res) => {
+  try {
+    res.status(200).json({ success: true, videos });
+  } catch (err) {
+    res.status(500).json({ success: false, msg: err.message });
+  }
+});
+
+app.get("/api/user", (req, res) => {
+  try {
+    res.status(200).json({ success: true, user });
+  } catch (err) {
+    res.status(500).json({ success: false, msg: err.message });
+  }
+});
 
 app.listen(5000, () => {
   console.log("Server is running at port:5000");
